12e94253df597ef2c17cb087ca1197d438818406
avarisc <avarisc@gmail.com>
2017-08-26 22:40:12 -0400

    Adding example project

---
+++ SDCExampleMod/SDCExampleMod.cs
@@ -0,0 +1,18 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDCExampleMod
+{
+    public class SDCExampleMod : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            StardewCustoms.CustomObject myObject = new StardewCustoms.CustomObject("exampleMod", "myObject");
+            StardewCustoms.CustomObjectHandler.RegisterCustomObject(myObject);
+        }
+    }
+}
